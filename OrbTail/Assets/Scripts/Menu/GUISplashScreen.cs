0f246a3123633fa40446f84bb3a72ac66f4ee693
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-04-14 22:28:38 +0200

    Slight refactoring of the splash screen.

--- OrbTail/Assets/Scripts/Menu/GUISplashScreen.cs
+++ OrbTail/Assets/Scripts/Menu/GUISplashScreen.cs
@@ -2,39 +2,33 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class GUISplashScreen : MonoBehaviour {
-    private float timeOfSplash = 1.5f;
-    private float fadeTime = 0.3f;
-    private GameObject radiance;
-    private GameObject polimi;
-    private GameObject menteZero;
-
-    // Use this for initialization
-    void Start () {
-        radiance = GameObject.Find("LogoRadiance");
-        polimi = GameObject.Find("PolimiGameCollective");
-        menteZero = GameObject.Find("MenteZero");
-
+public class GUISplashScreen : MonoBehaviour
+{
+    public GameObject radiance;
+    public GameObject polimi;
+    public GameObject mente_zero;
+    
+    void Start ()
+    {
         iTween.FadeTo(polimi, 0f, 0f);
-        iTween.FadeTo(menteZero, 0f, 0f);
+        iTween.FadeTo(mente_zero, 0f, 0f);
+
         StartCoroutine("ChangeSecondSplash");
     }
     
-    // Update is called once per frame
-    void Update () {
-    
-    }
-
-    private IEnumerator ChangeSecondSplash() {
+    private IEnumerator ChangeSecondSplash()
+    {
         yield return new WaitForSeconds(timeOfSplash);
-        polimi.GetComponent<Renderer>().enabled = true;
-        menteZero.GetComponent<Renderer>().enabled = true;
+
         iTween.FadeTo(radiance, 0f, fadeTime);
         iTween.FadeTo(polimi, 1f, fadeTime);
-        iTween.FadeTo(menteZero, 1f, fadeTime);
-        yield return new WaitForSeconds(timeOfSplash + fadeTime);
-        SceneManager.LoadScene("MenuMain", LoadSceneMode.Single);
+        iTween.FadeTo(mente_zero, 1f, fadeTime);
 
+        yield return new WaitForSeconds(timeOfSplash + fadeTime);
+
+        SceneManager.LoadScene("MenuMain", LoadSceneMode.Single);
     }
-    
+
+    private float timeOfSplash = 1.5f;
+    private float fadeTime = 0.3f;
 }
