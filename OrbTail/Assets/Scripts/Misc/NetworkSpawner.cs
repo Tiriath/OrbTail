2634419af270c69003b47858f9926c052ac2b86b
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-05-01 23:05:27 +0200

    Adding support to networked spawn of orbs. Updating PlanArena using the new spawning system.

---
+++ OrbTail/Assets/Scripts/Misc/NetworkSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Script used to spawn a networked object on the server and each client.
+/// </summary>
+public class NetworkSpawner : NetworkBehaviour
+{
+    /// <summary>
+    /// Prefab to spawn.
+    /// </summary>
+    public GameObject prefab;
+
+    /// <summary>
+    /// Whether to automatically destroy the object after spawning.
+    /// </summary>
+    public bool auto_destroy = true;
+
+    /// <summary>
+    /// Called on the server.
+    /// </summary>
+    public override void OnStartServer()
+    {
+        GameObject instance = Instantiate(prefab, transform.position, transform.localRotation);
+
+        NetworkServer.Spawn(instance);
+
+        if(auto_destroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
