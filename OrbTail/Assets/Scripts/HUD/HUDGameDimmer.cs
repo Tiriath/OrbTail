b1543de7aa9794a9fa3bec67bfdefc3f656601dc
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-05-10 21:44:52 +0200

    Adding support to networked game end.

---
+++ OrbTail/Assets/Scripts/HUD/HUDGameDimmer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// HUD element used to dim the interface when the match ends.
+/// </summary>
+public class HUDGameDimmer : HUDElement
+{
+    /// <summary>
+    /// Time fade duration.
+    /// </summary>
+    public float fade_duration = 0.3f;
+
+    /// <summary>
+    /// Fade easing curve.
+    /// </summary>
+    public iTween.EaseType fade_ease = iTween.EaseType.easeInCubic;
+
+    public void Awake()
+    {
+        gameObject.SetActive(false);
+
+        BaseGameMode.Instance.MatchEndEvent += OnMatchEnd;
+    }
+
+    public void OnDestroy()
+    {
+        BaseGameMode.Instance.MatchEndEvent -= OnMatchEnd;
+    }
+
+    /// <summary>
+    /// Called whenever the game mode ends.
+    /// </summary>
+    private void OnMatchEnd(BaseGameMode game_mode)
+    {
+        gameObject.SetActive(true);
+
+        BaseGameMode.Instance.MatchCountdownEvent -= OnMatchEnd;
+
+        iTween.FadeFrom(gameObject, 0.0f, fade_duration);
+    }
+}
---
+++ OrbTail/Assets/Scripts/HUD/HUDShipOutcome.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// HUD element used to show the outcome of a match.
+/// </summary>
+public class HUDShipOutcome : HUDElement
+{
+    /// <summary>
+    /// Message to show when a player wins.
+    /// </summary>
+    public string win_message = "{0} wins!";
+
+    /// <summary>
+    /// Message to show when the match ended with a tie.
+    /// </summary>
+    public string tie_message = "tie";
+
+    public void Awake()
+    {
+        text_mesh = gameObject.GetComponent<TextMesh>();
+
+        BaseGameMode.Instance.MatchEndEvent += OnMatchEnd;
+    }
+
+    public void OnDestroy()
+    {
+        BaseGameMode.Instance.MatchEndEvent -= OnMatchEnd;
+    }
+
+    /// <summary>
+    /// Called whenever the game mode ends.
+    /// </summary>
+    private void OnMatchEnd(BaseGameMode game_mode)
+    {
+        BaseGameMode.Instance.MatchCountdownEvent -= OnMatchEnd;
+
+        text_mesh.text = "Game over";
+    }
+
+    /// <summary>
+    /// Element used to displayer the game time on.
+    /// </summary>
+    private TextMesh text_mesh;
+}
