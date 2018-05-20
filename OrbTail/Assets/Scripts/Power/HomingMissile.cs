0148563fbb4eaf9ac311d805550b998c4503218a
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-05-20 16:31:58 +0200

    Adding projectile power-up. Tuning homing-missile and boost power-ups.

--- OrbTail/Assets/Scripts/Power/HomingMissile.cs
+++ OrbTail/Assets/Scripts/Power/HomingMissile.cs
@@ -4,47 +4,16 @@ using UnityEngine.Networking;
 /// <summary>
 /// A homing missile that lock on a target.
 /// </summary>
-public class HomingMissile : PowerUpEffect
+public class HomingMissile : Projectile
 {
-    /// <summary>
-    /// Explosion prefab.
-    /// </summary>
-    public GameObject explosion;
-
-    /// <summary>
-    /// Explosion force on impact.
-    /// </summary>
-    public float explosion_force = 4.0f;
-
     /// <summary>
     /// Maximum missile steering.
     /// </summary>
     public float steering = 6.0f;
 
-    /// <summary>
-    /// Missile forward speed.
-    /// </summary>
-    public float speed = 8.0f;
-
-    /// <summary>
-    /// Number of orbs detached upon impact.
-    /// </summary>
-    public int damage = 2;
-
-    public void OnDestroy()
-    {
-        if(explosion)
-        {
-            Instantiate(explosion, transform.position, Quaternion.identity);
-        }
-    }
-
     public override void OnStartClient()
     {
         base.OnStartClient();
-
-        rigid_body = GetComponent<Rigidbody>();
-        floating_object = GetComponent<FloatingObject>();
 
         // Lock onto the nearest target.
 
@@ -79,44 +48,7 @@ public class HomingMissile : PowerUpEffect
 
             transform.rotation = Quaternion.LookRotation(forward, floating_object.Up);
         }
-
-        // Propel the missile forward.
-
-        rigid_body.AddForce(transform.forward * speed, ForceMode.VelocityChange);
     }
-
-    /// <summary>
-    /// Called whenever the missile hits something.
-    /// </summary>
-    void OnTriggerEnter(Collider collision)
-    {
-        if(isServer)
-        {
-            var ship = collision.gameObject.GetComponent<Ship>();
-
-            if (ship && ship != Owner)
-            {
-                ship.GetComponent<Rigidbody>().AddForce(transform.forward * explosion_force, ForceMode.VelocityChange);
-
-                for(int number = 0; number < damage; ++number)
-                {
-                    ship.RpcDetachOrb();
-                }
-
-                Destroy(gameObject);
-            }
-        }
-    }
-    
-    /// <summary>
-    /// Missile rigid body.
-    /// </summary>
-    private Rigidbody rigid_body;
-
-    /// <summary>
-    /// Missile floating object.
-    /// </summary>
-    private FloatingObject floating_object;
 
     /// <summary>
     /// Target of this missile.
---
+++ OrbTail/Assets/Scripts/Power/Projectile.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// A propelled projectile which deals damage upon impact.
+/// </summary>
+public class Projectile : PowerUpEffect
+{
+    /// <summary>
+    /// Explosion prefab.
+    /// </summary>
+    public GameObject explosion;
+
+    /// <summary>
+    /// Explosion force on impact.
+    /// </summary>
+    public float explosion_force = 4.0f;
+
+    /// <summary>
+    /// Projectile forward speed.
+    /// </summary>
+    public float speed = 8.0f;
+
+    /// <summary>
+    /// Number of orbs detached upon impact.
+    /// </summary>
+    public int damage = 3;
+
+    public void OnDestroy()
+    {
+        if (explosion)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        rigid_body = GetComponent<Rigidbody>();
+        floating_object = GetComponent<FloatingObject>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        rigid_body.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+    }
+
+    /// <summary>
+    /// Called whenever the missile hits something.
+    /// </summary>
+    void OnTriggerEnter(Collider collision)
+    {
+        if (isServer)
+        {
+            var ship = collision.gameObject.GetComponent<Ship>();
+
+            if (ship && ship != Owner)
+            {
+                ship.GetComponent<Rigidbody>().AddForce(transform.forward * explosion_force, ForceMode.VelocityChange);
+
+                for (int number = 0; number < damage; ++number)
+                {
+                    ship.RpcDetachOrb();
+                }
+
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Missile rigid body.
+    /// </summary>
+    protected Rigidbody rigid_body;
+
+    /// <summary>
+    /// Missile floating object.
+    /// </summary>
+    protected FloatingObject floating_object;
+}
