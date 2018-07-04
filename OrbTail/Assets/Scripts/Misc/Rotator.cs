a1681f39812f78c3cec3cfc3a781f9b329c46a29
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-07-04 20:05:58 +0200

    Improving ship selection menu page via 3D models of various ships.

---
+++ OrbTail/Assets/Scripts/Misc/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script used to automatically rotate the game object.
+/// </summary>
+public class Rotator : MonoBehaviour
+{
+    /// <summary>
+    /// Rotation to add per second, in degrees for each axis.
+    /// </summary>
+    public Vector3 rotation;
+    
+    void Update ()
+    {
+        var delta_rotation = rotation * Time.deltaTime;
+
+        transform.localRotation *= Quaternion.Euler(delta_rotation.x, delta_rotation.y, delta_rotation.z);
+    }
+}
