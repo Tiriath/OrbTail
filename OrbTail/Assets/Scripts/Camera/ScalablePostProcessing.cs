d9fba4bf1ead9c6e7b0ba6a3afe3ac1820cf81eb
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-07-09 22:32:50 +0200

    Fine-tuning performance on mobile platform to target 30 FPS minimum.

---
+++ OrbTail/Assets/Scripts/Camera/ScalablePostProcessing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+/// <summary>
+/// Post processing component that can be disabled according to current quality levels.
+/// </summary>
+public class ScalablePostProcessing : PostProcessingBehaviour
+{
+    /// <summary>
+    /// Quality level below of which post processing is disabled.
+    /// </summary>
+    int quality_level_threshold = 0;
+
+    void Start ()
+    {
+        // Kill any post process when the quality level is low enough (like on mobile)
+
+        int quality_level = QualitySettings.GetQualityLevel();
+
+        enabled = (quality_level > quality_level_threshold);
+    }
+}
